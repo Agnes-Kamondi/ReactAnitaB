8a90e929976398615a5bedcd2fdce81ff289dde8
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-06-21 17:26:02 +0300

    react API

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function About() {
+    return (
+        <div className="about-text">
+            <p>We are learning about JSX</p>
+            <p>We are coding a new front-end library</p>
+        </div>
+    );
+}
+export default About;
